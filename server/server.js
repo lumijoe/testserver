48d834cecbd860fd422bd277631c2aa92e8694d0
lumijoe <spacedesign63@gmail.com>
2023-12-20 19:00:07 +0900

    set server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// server/server.js
+// バックエンドサーバーExpressの準備（API）
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 3004;
+
+app.use(cors());
+
+app.get("/api/home", (req, res) => {
+    res.json({ message: "This is server.js" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
